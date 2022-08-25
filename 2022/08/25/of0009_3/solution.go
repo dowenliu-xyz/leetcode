3265536527be2ec28f1746606ffcceb23f12c545
dowenliu-xyz <hawkdowen@hotmail.com>
2022-08-25 13:39:50 +0800

    WIP: 剑指Offer 09, the third time

---
+++ 2022/08/25/of0009_3/solution.go
@@ -0,0 +1,29 @@
+package main
+
+type CQueue struct {
+	in  []int
+	out []int
+}
+
+func Constructor() CQueue {
+	return CQueue{}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.in = append(this.in, value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if len(this.out) == 0 {
+		for len(this.in) > 0 {
+			this.out = append(this.out, this.in[len(this.in)-1])
+			this.in = this.in[:len(this.in)-1]
+		}
+	}
+	if len(this.out) == 0 {
+		return -1
+	}
+	ans := this.out[len(this.out)-1]
+	this.out = this.out[:len(this.out)-1]
+	return ans
+}
