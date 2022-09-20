98d09a3c665145bd07d517ca4071e2492f7a66f2
dowenliu-xyz <hawkdowen@hotmail.com>
2022-09-20 22:50:05 +0800

    WIP: leetcode 102, the fourth time

---
+++ 2022/09/20/lc0102_4/solution_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestLevelOrder(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: "[3,9,20,null,null,15,7]",
+			want:  "[[3],[9,20],[15,7]]",
+		},
+		{
+			input: "[1]",
+			want:  "[[1]]",
+		},
+		{
+			input: "[]",
+			want:  "[]",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			root := levelOrderRestore(parseStrings(tt.input))
+			result := levelOrder(root)
+			got := formatResult(result)
+			if tt.want != got {
+				t.Errorf("want %s, got %s", tt.want, got)
+			}
+		})
+	}
+}
