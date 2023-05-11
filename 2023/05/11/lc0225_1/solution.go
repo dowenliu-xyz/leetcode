2752aafe565e675318d1ea3159011f0781f16a00
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-11 20:55:41 +0800

    WIP: leetcode 225, the first time

---
+++ 2023/05/11/lc0225_1/solution.go
@@ -0,0 +1,45 @@
+package lc0225_1
+
+type MyStack struct {
+	q1 []int
+	q2 []int
+}
+
+func Constructor() MyStack {
+	return MyStack{}
+}
+
+func (this *MyStack) Push(x int) {
+	this.q1 = append(this.q1, x)
+}
+
+func (this *MyStack) Pop() int {
+	for len(this.q1) > 1 {
+		this.q2 = append(this.q2, this.q1[0])
+		this.q1 = this.q1[1:]
+	}
+	v := this.q1[0]
+	this.q1, this.q2 = this.q2, nil
+	return v
+}
+
+func (this *MyStack) Top() int {
+	for len(this.q1) > 1 {
+		this.q2 = append(this.q2, this.q1[0])
+		this.q1 = this.q1[1:]
+	}
+	return this.q1[0]
+}
+
+func (this *MyStack) Empty() bool {
+	return len(this.q1) == 0 && len(this.q2) == 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
