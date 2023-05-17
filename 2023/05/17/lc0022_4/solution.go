463d7c8a259df2403525acaa340804b6a38754ad
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-17 21:36:25 +0800

    WIP: leetcode 22, the fourth time

---
+++ 2023/05/17/lc0022_4/solution.go
@@ -0,0 +1,20 @@
+package lc0022_4
+
+func generateParenthesis(n int) []string {
+	var ans []string
+	var dfs func(ans *[]string, left, right, n int, data []byte)
+	dfs = func(ans *[]string, left, right, n int, data []byte) {
+		if left == right && left == n {
+			*ans = append(*ans, string(data))
+			return
+		}
+		if left < n {
+			dfs(ans, left+1, right, n, append(data, '('))
+		}
+		if left > right {
+			dfs(ans, left, right+1, n, append(data, ')'))
+		}
+	}
+	dfs(&ans, 0, 0, n, nil)
+	return ans
+}
---
+++ 2023/05/17/lc0022_4/solution_test.go
@@ -0,0 +1,40 @@
+package lc0022_4
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+func Test_generateParenthesis(t *testing.T) {
+	tests := []struct {
+		input int
+		want  string
+	}{
+		{input: 1, want: `["()"]`},
+		{input: 2, want: `["(())","()()"]`},
+		{input: 3, want: `["((()))","(()())","(())()","()(())","()()()"]`},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			got := generateParenthesis(tt.input)
+			if got := sprintStrings(got); got != tt.want {
+				t.Errorf("generateParenthesis(%d) = %s, want %s", tt.input, got, tt.want)
+			}
+		})
+	}
+}
+
+func sprintStrings(ss []string) string {
+	buf := bytes.Buffer{}
+	buf.WriteByte('[')
+	for i, s := range ss {
+		if i > 0 {
+			buf.WriteByte(',')
+		}
+		buf.WriteString(strconv.Quote(s))
+	}
+	buf.WriteByte(']')
+	return buf.String()
+}
