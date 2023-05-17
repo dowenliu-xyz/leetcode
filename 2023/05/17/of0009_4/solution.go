94229dcaf93fc44ae9bd63e6b5d439985ab440ee
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-17 13:50:42 +0800

    WIP: 剑指offer 9, the fourth time

---
+++ 2023/05/17/of0009_4/solution.go
@@ -0,0 +1,35 @@
+package of0009_4
+
+type CQueue struct {
+	input, output []int
+}
+
+func Constructor() CQueue {
+	return CQueue{}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.input = append(this.input, value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if len(this.input) == 0 && len(this.output) == 0 {
+		return -1
+	}
+	if len(this.output) == 0 {
+		for len(this.input) > 0 {
+			this.output = append(this.output, this.input[len(this.input)-1])
+			this.input = this.input[:len(this.input)-1]
+		}
+	}
+	ans := this.output[len(this.output)-1]
+	this.output = this.output[:len(this.output)-1]
+	return ans
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.AppendTail(value);
+ * param_2 := obj.DeleteHead();
+ */
---
+++ 2023/05/17/of0009_4/solution_test.go
@@ -0,0 +1,127 @@
+package of0009_4
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/dowenliu-xyz/leetcode/internal/inputs"
+)
+
+func TestCQueue(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: `
+["CQueue","appendTail","deleteHead","deleteHead","deleteHead"]
+[[],[3],[],[],[]]
+`,
+			want: `[null,null,3,-1,-1]`,
+		},
+		{
+			input: `
+["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+[[],[],[5],[2],[],[]]
+`,
+			want: `[null,-1,null,null,5,2]`,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			operations, args, err := readInput(tt.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			var result []any
+			q := &CQueue{}
+			for j, operation := range operations {
+				op, ok := opMap[operation]
+				if !ok {
+					t.Fatalf("unexpected operation: %s", operation)
+				}
+				opResult := op(t, q, args[j])
+				result = append(result, opResult)
+			}
+			got := sprintResult(result)
+			want := strings.ReplaceAll(tt.want, " ", "")
+			if got != want {
+				t.Errorf("expect %s, got %s", want, got)
+			}
+		})
+	}
+}
+
+func readInput(input string) (operations []string, args []string, err error) {
+	lines := inputs.ReadNoneBlankLines(input)
+	if len(lines) != 2 {
+		err = fmt.Errorf("input should have 2 lines, but got %d", len(lines))
+		return
+	}
+	operations, err = inputs.ReadStringSlice(lines[0])
+	if err != nil {
+		return
+	}
+	args, err = readArgs(lines[1])
+	if err != nil {
+		return
+	}
+	if len(operations) != len(args) {
+		err = fmt.Errorf("len(operations): %d, but len(args): %d", len(operations), len(args))
+		return
+	}
+	return
+}
+
+func readArgs(input string) ([]string, error) {
+	input = strings.ReplaceAll(input, " ", "")
+	if !strings.HasPrefix(input, "[[") || !strings.HasSuffix(input, "]]") {
+		return nil, fmt.Errorf("input should be wrapped by '[[' and ']]'")
+	}
+	input = input[2 : len(input)-2]
+	ans := strings.Split(input, "],[")
+	return ans, nil
+}
+
+func sprintResult(result []any) string {
+	var sb strings.Builder
+	sb.WriteString("[")
+	for i := range result {
+		if i != 0 {
+			sb.WriteString(",")
+		}
+		if result[i] == nil {
+			sb.WriteString("null")
+			continue
+		}
+		sb.WriteString(fmt.Sprintf("%v", result[i]))
+	}
+	sb.WriteString("]")
+	return sb.String()
+}
+
+var opMap = map[string]func(t *testing.T, q *CQueue, args string) any{
+	"CQueue":     callConstruct,
+	"appendTail": callAppendTail,
+	"deleteHead": callDeletedHead,
+}
+
+func callConstruct(_ *testing.T, q *CQueue, _ string) any {
+	*q = Constructor()
+	return nil
+}
+
+func callAppendTail(t *testing.T, q *CQueue, args string) any {
+	value, err := strconv.Atoi(args)
+	if err != nil {
+		t.Fatalf("invalid args: %v", args)
+	}
+	q.AppendTail(value)
+	return nil
+}
+
+func callDeletedHead(_ *testing.T, q *CQueue, _ string) any {
+	return q.DeleteHead()
+}
