ec5bcf90c815ccc6425bfb107e0b46af6a67ab3a
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-18 21:59:56 +0800

    WIP: leetcode 14, the first time (restarted)

---
+++ 2023/05/18/lc0014_1/solution.go
@@ -0,0 +1,16 @@
+package lc0014_1
+
+func longestCommonPrefix(strs []string) string {
+	n := len(strs)
+	if n == 0 {
+		return ""
+	}
+	for i := range strs[0] {
+		for j := 1; j < n; j++ {
+			if i == len(strs[j]) || strs[0][i] != strs[j][i] {
+				return strs[j][:i]
+			}
+		}
+	}
+	return strs[0]
+}
---
+++ 2023/05/18/lc0014_1/solution_test.go
@@ -0,0 +1,35 @@
+package lc0014_1
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dowenliu-xyz/leetcode/internal/inputs"
+)
+
+func Test_longestCommonPrefix(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{
+			input: `["flower","flow","flight"]`,
+			want:  "fl",
+		},
+		{
+			input: `["dog","racecar","car"]`,
+			want:  "",
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			strs, err := inputs.ReadStringSlice(tt.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got := longestCommonPrefix(strs); got != tt.want {
+				t.Errorf("longestCommonPrefix() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
