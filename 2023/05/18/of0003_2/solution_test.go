933b18c4249dd63c4b35b7a25c3e8357f6c6e8ce
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-18 14:00:59 +0800

    WIP: 剑指offer 3, the second time

---
+++ 2023/05/18/of0003_2/solution_test.go
@@ -0,0 +1,28 @@
+package of0003_2
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dowenliu-xyz/leetcode/internal/inputs"
+)
+
+func Test_findRepeatNumber(t *testing.T) {
+	tests := []struct {
+		input string
+		want  int
+	}{
+		{"[2, 3, 1, 0, 2, 5, 3]", 2},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			nums, err := inputs.ReadIntSlice(tt.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got := findRepeatNumber(nums); got != tt.want {
+				t.Errorf("findRepeatNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
