4eb89fb2feff4437bb583a2776f6c7293f0e486d
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-18 12:57:15 +0800

    WIP: leetcode 15, the second time

---
+++ 2023/05/18/lc0015_2/solution.go
@@ -0,0 +1,68 @@
+package lc0015_2
+
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	//return solution1(nums)
+	return solution2(nums)
+}
+
+func solution1(nums []int) [][]int {
+	n := len(nums)
+	if n < 3 {
+		return nil
+	}
+	var ans [][]int
+	ansSet, seen := map[[3]int]bool{}, map[int]bool{nums[0]: true}
+	for i := 1; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			target := 0 - nums[i] - nums[j]
+			if seen[target] {
+				array := [3]int{nums[i], nums[j], target}
+				sort.Ints(array[:])
+				if ansSet[array] {
+					continue
+				}
+				ans = append(ans, array[:])
+				ansSet[array] = true
+			}
+		}
+		seen[nums[i]] = true
+	}
+	return ans
+}
+
+func solution2(nums []int) [][]int {
+	n := len(nums)
+	if n < 3 {
+		return nil
+	}
+	sort.Ints(nums)
+	var ans [][]int
+	for i := 0; i < n; i++ {
+		if nums[i] > 0 {
+			break
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, n-1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum == 0 {
+				ans = append(ans, []int{nums[i], nums[l], nums[r]})
+				for ; l < r && nums[l] == nums[l+1]; l++ {
+				}
+				for ; l < r && nums[r] == nums[r-1]; r-- {
+				}
+				l++
+				r--
+			} else if sum < 0 {
+				l++
+			} else {
+				r--
+			}
+		}
+	}
+	return ans
+}
