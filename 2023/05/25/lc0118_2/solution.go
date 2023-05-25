e3349ec1bb290965225bc9ca89d035851a646f26
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-25 21:34:31 +0800

    WIP: leetcode 118, the second time

---
+++ 2023/05/25/lc0118_2/solution.go
@@ -0,0 +1,17 @@
+package lc0118_2
+
+func generate(numRows int) [][]int {
+	if numRows == 0 {
+		return nil
+	}
+	ans := make([][]int, numRows)
+	ans[0] = []int{1}
+	for i := 1; i < numRows; i++ {
+		ans[i] = make([]int, i+1)
+		ans[i][0], ans[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			ans[i][j] = ans[i-1][j-1] + ans[i-1][j]
+		}
+	}
+	return ans
+}
