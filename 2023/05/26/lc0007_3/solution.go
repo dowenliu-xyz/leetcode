14e9cfdc9b980528606665bd810b70fbe9047e9b
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-26 12:59:46 +0800

    WIP: leetcode 7, the third time

---
+++ 2023/05/26/lc0007_3/solution.go
@@ -0,0 +1,21 @@
+package lc0007_3
+
+import "math"
+
+func reverse(x int) int {
+	maxQ, maxR := math.MaxInt32/10, math.MaxInt32%10
+	minQ, minR := math.MinInt32/10, math.MinInt32%10
+	ans := 0
+	for x != 0 {
+		tmp := x % 10
+		if x < 0 && (ans < minQ || (ans == minQ && tmp < minR)) {
+			return 0
+		}
+		if x > 0 && (ans > maxQ || (ans == maxQ && tmp > maxR)) {
+			return 0
+		}
+		ans = ans*10 + tmp
+		x /= 10
+	}
+	return ans
+}
