a3eb76abea95f606a8939517aa1e7f70b7ae9824
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-26 13:27:23 +0800

    WIP: leetcode 45, the third time

---
+++ 2023/05/26/lc0045_3/solution.go
@@ -0,0 +1,24 @@
+package lc0045_3
+
+func jump(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return -1
+	}
+	ans, leave, reachable := 0, 0, 0
+	for i := 0; i < n-1; i++ {
+		reachable = max(reachable, i+nums[i])
+		if i == leave {
+			ans++
+			leave = reachable
+		}
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
