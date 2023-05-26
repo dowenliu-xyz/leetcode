343ae3aa7868bbc281f26de9ac51a30a9b550425
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-26 14:01:18 +0800

    WIP: leetcode 322, the third time

---
+++ 2023/05/26/lc0322_3/solution.go
@@ -0,0 +1,70 @@
+package lc0322_3
+
+import "math"
+
+func coinChange(coins []int, amount int) int {
+	//return solution1(coins, amount, make([]int, amount+1))
+	return solution2(coins, amount)
+}
+
+func solution1(coins []int, amount int, mem []int) int {
+	if amount < 0 {
+		return -1
+	}
+	if amount == 0 {
+		return 0
+	}
+	if mem[amount] != 0 {
+		return mem[amount]
+	}
+	minUse := math.MaxInt
+	for _, coin := range coins {
+		use := solution1(coins, amount-coin, mem)
+		if use == -1 {
+			continue
+		}
+		if use < minUse {
+			minUse = use
+		}
+	}
+	if minUse == math.MaxInt {
+		mem[amount] = -1
+	} else {
+		mem[amount] = minUse + 1
+	}
+	return mem[amount]
+}
+
+func solution2(coins []int, amount int) int {
+	if amount < 0 {
+		return -1
+	}
+	if amount == 0 {
+		return 0
+	}
+	dp := make([]int, amount+1)
+	dp[0] = amount + 1
+	for i := 1; i < len(dp); i = i << 1 {
+		copy(dp[i:], dp[:i])
+	}
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		for _, coin := range coins {
+			if i < coin {
+				continue
+			}
+			dp[i] = min(dp[i], dp[i-coin]+1)
+		}
+	}
+	if dp[amount] <= amount {
+		return dp[amount]
+	}
+	return -1
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ 2023/05/26/lc0322_3/solution_test.go
@@ -0,0 +1,81 @@
+package lc0322_3
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/dowenliu-xyz/leetcode/internal/inputs"
+)
+
+func TestCoinChange(t *testing.T) {
+	tests := []struct {
+		input string
+		want  int
+	}{
+		{
+			input: `
+[1,2,5]
+11
+`,
+			want: 3,
+		},
+		{
+			input: `
+[2]
+3
+`,
+			want: -1,
+		},
+		{
+			input: `
+[1]
+0
+`,
+			want: 0,
+		},
+		{
+			input: `
+[186,419,83,408]
+6249
+`,
+			want: 20,
+		},
+		{
+			input: `
+[1]
+1
+`,
+			want: 1,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			coins, amount := parseInput(t, tt.input)
+			got := coinChange(coins, amount)
+			if got != tt.want {
+				t.Errorf("expect %d, got %d", tt.want, got)
+			}
+		})
+	}
+}
+
+func parseInput(t *testing.T, input string) (coins []int, amount int) {
+	t.Helper()
+
+	lines := inputs.ReadNoneBlankLines(input)
+	if len(lines) != 2 {
+		t.Fatalf("expect 2 lines, got %d", len(lines))
+	}
+	var err error
+	coins, err = inputs.ReadIntSlice(lines[0])
+	if err != nil {
+		t.Fatal(err)
+	}
+	i, err := strconv.ParseInt(lines[1], 10, 64)
+	if err != nil {
+		t.Fatal(err)
+	}
+	amount = int(i)
+	return
+}
