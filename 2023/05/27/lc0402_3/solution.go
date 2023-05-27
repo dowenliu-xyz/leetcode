1ed2afbe0aa520598aafe69257d61235ed4c23d6
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-27 13:47:07 +0800

    WIP: leetcode 402, the third time

---
+++ 2023/05/27/lc0402_3/solution.go
@@ -0,0 +1,22 @@
+package lc0402_3
+
+func removeKdigits(num string, k int) string {
+	var stack []byte
+	for i := 0; i < len(num); i++ {
+		for k > 0 && len(stack) > 0 && stack[len(stack)-1] > num[i] {
+			stack = stack[:len(stack)-1]
+			k--
+		}
+		stack = append(stack, num[i])
+	}
+	if k > 0 {
+		stack = stack[:len(stack)-k]
+	}
+	for len(stack) > 0 && stack[0] == '0' {
+		stack = stack[1:]
+	}
+	if len(stack) == 0 {
+		return "0"
+	}
+	return string(stack)
+}
