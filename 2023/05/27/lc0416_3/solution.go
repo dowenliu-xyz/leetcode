f28be2c5ba625a21632fb9094c7f01b5f7a3739b
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-27 23:35:26 +0800

    WIP: leetcode 416, the third time

---
+++ 2023/05/27/lc0416_3/solution.go
@@ -0,0 +1,46 @@
+package lc0416_3
+
+func canPartition(nums []int) bool {
+	n := len(nums)
+	if n < 2 {
+		return false
+	}
+	sum, max := 0, 0
+	for _, num := range nums {
+		sum += num
+		if num > max {
+			max = num
+		}
+	}
+	if sum&1 == 1 {
+		return false
+	}
+	target := sum >> 1
+	if max > target {
+		return false
+	}
+	//dp := make([][]bool, n)
+	//for i := range dp {
+	//	dp[i] = make([]bool, target+1)
+	//	dp[i][0] = true
+	//}
+	//dp[0][nums[0]] = true
+	//for i := 1; i < n; i++ {
+	//	for j := 1; j <= target; j++ {
+	//		if j < nums[i] {
+	//			dp[i][j] = dp[i-1][j]
+	//		} else {
+	//			dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i]]
+	//		}
+	//	}
+	//}
+	//return dp[n-1][target]
+	dp := make([]bool, target+1)
+	dp[0] = true
+	for _, num := range nums {
+		for i := target; i >= num; i-- {
+			dp[i] = dp[i] || dp[i-num]
+		}
+	}
+	return dp[target]
+}
---
+++ 2023/05/27/lc0416_3/solution_test.go
@@ -0,0 +1,44 @@
+package lc0416_3
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dowenliu-xyz/leetcode/internal/inputs"
+)
+
+func TestCanPartition(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{
+			input: `[1,5,11,5]`,
+			want:  true,
+		},
+		{
+			input: `[1,2,3,5]`,
+			want:  false,
+		},
+		{
+			input: `[2,2,3,5]`,
+			want:  false,
+		},
+		{
+			input: `[3,3,3,4,5]`,
+			want:  true,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			nums, err := inputs.ReadIntSlice(tt.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			got := canPartition(nums)
+			if tt.want != got {
+				t.Fatalf("expect %t, got %t", tt.want, got)
+			}
+		})
+	}
+}
