89873f937e08f9799c806991d864ec4bc28e24b8
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-28 17:19:15 +0800

    WIP: leetcode 206, the second time

---
+++ 2023/05/28/lc0206_2/solution.go
@@ -0,0 +1,16 @@
+package lc0206_2
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	for head != nil {
+		pre, head, head.Next = head, head.Next, pre
+	}
+	return pre
+}
---
+++ 2023/05/28/lc0206_2/type.go
@@ -0,0 +1,6 @@
+package lc0206_2
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
