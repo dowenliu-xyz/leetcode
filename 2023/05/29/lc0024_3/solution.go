49909c43d595b44b34d16a4702894e0b74793766
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-29 22:11:55 +0800

    WIP: leetcode 24, the third time

---
+++ 2023/05/29/lc0024_3/solution.go
@@ -0,0 +1,18 @@
+package lc0024_3
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func swapPairs(head *ListNode) *ListNode {
+	dummy := &ListNode{Next: head}
+	pre := dummy
+	for head != nil && head.Next != nil {
+		pre.Next, head.Next, head.Next.Next = head.Next, head.Next.Next, head
+		pre, head = head, head.Next
+	}
+	return dummy.Next
+}
---
+++ 2023/05/29/lc0024_3/type.go
@@ -0,0 +1,6 @@
+package lc0024_3
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
