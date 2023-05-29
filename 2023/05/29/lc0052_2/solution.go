deb27e74b625f43a3b587498a9c6cd8c91ee41b7
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-29 20:31:47 +0800

    WIP: leetcode 52, the second time

---
+++ 2023/05/29/lc0052_2/solution.go
@@ -0,0 +1,26 @@
+package lc0052_2
+
+import "math/bits"
+
+func totalNQueens(n int) int {
+	if n == 0 {
+		return 0
+	}
+	return solve(n, make([]int, 0, n), 0, 0, 0)
+}
+
+func solve(n int, queens []int, colBits, pieBits, naBits uint) int {
+	if len(queens) == n {
+		return 1
+	}
+	invalidBits := colBits | pieBits | naBits
+	possibleBits := (uint(1)<<n - 1) & (^invalidBits)
+	ans := 0
+	for possibleBits != 0 {
+		pos := possibleBits & (-possibleBits)
+		possibleBits = possibleBits & (possibleBits - 1)
+		col := bits.OnesCount(pos - 1)
+		ans += solve(n, append(queens, col), colBits|pos, (pieBits|pos)<<1, (naBits|pos)>>1)
+	}
+	return ans
+}
