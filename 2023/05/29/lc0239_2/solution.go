9795434bc5795a33e983938363f2aeac05f3c631
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-29 22:48:31 +0800

    WIP: leetcode 239, the second time

---
+++ 2023/05/29/lc0239_2/solution.go
@@ -0,0 +1,20 @@
+package lc0239_2
+
+func maxSlidingWindow(nums []int, k int) []int {
+	var win []int
+	var ans []int
+	for i := 0; i < len(nums); i++ {
+		for len(win) > 0 && nums[win[len(win)-1]] <= nums[i] {
+			win = win[:len(win)-1]
+		}
+		win = append(win, i)
+		if i < k-1 {
+			continue
+		}
+		if win[0] <= i-k {
+			win = win[1:]
+		}
+		ans = append(ans, nums[win[0]])
+	}
+	return ans
+}
