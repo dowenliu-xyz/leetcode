c1eb7b6e3a1ce5e7bd4437e4cea0b7fc13db1021
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-30 18:15:21 +0800

    WIP: leetcode 101, the second time

---
+++ 2023/05/30/lc0101_2/solution.go
@@ -0,0 +1,26 @@
+package lc0101_2
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	return isMirror(root, root)
+}
+
+func isMirror(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	if left.Val != right.Val {
+		return false
+	}
+	return isMirror(left.Left, right.Right) && isMirror(left.Right, right.Left)
+}
---
+++ 2023/05/30/lc0101_2/type.go
@@ -0,0 +1,7 @@
+package lc0101_2
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
