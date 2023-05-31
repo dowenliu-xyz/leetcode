e8b33cac5f9adf89e9356465ab19088ad860d749
dowenliu-xyz <hawkdowen@hotmail.com>
2023-05-31 13:00:38 +0800

    WIP: leetcode 278, the second time

---
+++ 2023/05/31/lc0278/solution.go
@@ -0,0 +1,24 @@
+package lc0278
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	return binarySearch(1, n)
+}
+
+func binarySearch(l, r int) int {
+	m := l + (r-l)>>1
+	if isBadVersion(m) {
+		if m == 1 || !isBadVersion(m-1) {
+			return m
+		}
+		return binarySearch(l, m-1)
+	}
+	return binarySearch(m+1, r)
+}
