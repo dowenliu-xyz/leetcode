1cf4ef7a259df9bc35f0230fe17ffe274337f816
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-01 12:24:31 +0800

    WIP: leetcode 10, the third time

---
+++ 2023/06/01/lc0010_3/solution.go
@@ -0,0 +1,38 @@
+package lc0010_3
+
+func isMatch(s string, p string) bool {
+	m, n := len(s), len(p)
+	dp := make([][]bool, m+1)
+	for i := range dp {
+		dp[i] = make([]bool, n+1)
+	}
+	dp[0][0] = true
+	for i := 2; i <= n; i++ {
+		if p[i-1] == '*' {
+			dp[0][i] = dp[0][i] || dp[0][i-2]
+		}
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if p[j-1] == '*' {
+				dp[i][j] = dp[i][j] || dp[i][j-2]
+				if match(s[i-1], p[j-2]) {
+					dp[i][j] = dp[i][j] || dp[i-1][j]
+				}
+			} else if match(s[i-1], p[j-1]) {
+				dp[i][j] = dp[i][j] || dp[i-1][j-1]
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func match(a, b byte) bool {
+	if a == b {
+		return true
+	}
+	if b == '.' {
+		return true
+	}
+	return false
+}
---
+++ 2023/06/01/lc0010_3/solution_test.go
@@ -0,0 +1,64 @@
+package lc0010_3
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dowenliu-xyz/leetcode/internal/inputs"
+)
+
+func TestIsMatch(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{
+			input: `
+aa
+a
+`,
+			want: false,
+		},
+		{
+			input: `
+aaa
+a*
+`,
+			want: true,
+		},
+		{
+			input: `
+ab
+.*
+`,
+			want: true,
+		},
+		{
+			input: `
+mississippi
+mis*is*ip*.
+`,
+			want: true,
+		},
+		{
+			input: `
+"mississippi"
+"mis*is*p*."
+`,
+			want: false,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			lines := inputs.ReadNoneBlankLines(tt.input)
+			if len(lines) != 2 {
+				t.Fatal("input should have 2 lines")
+			}
+			s := lines[0]
+			p := lines[1]
+			if got := isMatch(s, p); got != tt.want {
+				t.Errorf("isMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
