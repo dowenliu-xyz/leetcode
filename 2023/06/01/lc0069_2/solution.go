170f212e01a54c3eace0246fa79873fc73fb1c22
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-01 13:18:05 +0800

    WIP: leetcode 69, the second time

---
+++ 2023/06/01/lc0069_2/solution.go
@@ -0,0 +1,18 @@
+package lc0069_2
+
+func mySqrt(x int) int {
+	low, high := 0, x
+	for low <= high {
+		mid := low + (high-low)>>1
+		square := mid * mid
+		if square == x {
+			return mid
+		}
+		if square < x {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return high
+}
