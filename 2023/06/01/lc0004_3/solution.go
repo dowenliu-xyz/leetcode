9a50168be9f6fcbaac28987ef91e3efafcb6071c
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-01 08:55:00 +0800

    WIP: leetcode 4, the third time

---
+++ 2023/06/01/lc0004_3/solution.go
@@ -0,0 +1,41 @@
+package lc0004_3
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	total := len(nums1) + len(nums2)
+	if total&1 == 1 {
+		return float64(findKthElement(nums1, nums2, total>>1+1))
+	}
+	l, r := findKthElement(nums1, nums2, total>>1), findKthElement(nums1, nums2, total>>1+1)
+	return float64(l) + float64(r-l)/2
+}
+
+func findKthElement(nums1, nums2 []int, k int) int {
+	offset1, offset2 := 0, 0
+	for {
+		if offset1 == len(nums1) {
+			return nums2[offset2+k-1]
+		}
+		if offset2 == len(nums2) {
+			return nums1[offset1+k-1]
+		}
+		if k == 1 {
+			return min(nums1[offset1], nums2[offset2])
+		}
+		half := k >> 1
+		newOffset1, newOffset2 := min(offset1+half-1, len(nums1)-1), min(offset2+half-1, len(nums2)-1)
+		if nums1[newOffset1] < nums2[newOffset2] {
+			k -= newOffset1 - offset1 + 1
+			offset1 = newOffset1 + 1
+		} else {
+			k -= newOffset2 - offset2 + 1
+			offset2 = newOffset2 + 1
+		}
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ 2023/06/01/lc0004_3/solution_test.go
@@ -0,0 +1,65 @@
+package lc0004_3
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dowenliu-xyz/leetcode/internal/inputs"
+)
+
+func Test_findMedianSortedArrays(t *testing.T) {
+	tests := []struct {
+		input string
+		want  float64
+	}{
+		{
+			input: `
+[1,3]
+[2]
+`,
+			want: 2.0,
+		},
+		{
+			input: `
+[1,2]
+[3,4]
+`,
+			want: 2.5,
+		},
+		{
+			input: `
+[0,0]
+[0,0]
+`,
+			want: 0.0,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			nums1, nums2, err := readInput(tt.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			got := findMedianSortedArrays(nums1, nums2)
+			if got != tt.want {
+				t.Errorf("findMedianSortedArrays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func readInput(input string) ([]int, []int, error) {
+	lines := inputs.ReadNoneBlankLines(input)
+	if len(lines) != 2 {
+		return nil, nil, fmt.Errorf("input should have 2 lines")
+	}
+	nums1, err := inputs.ReadIntSlice(lines[0])
+	if err != nil {
+		return nil, nil, err
+	}
+	nums2, err := inputs.ReadIntSlice(lines[1])
+	if err != nil {
+		return nil, nil, err
+	}
+	return nums1, nums2, nil
+}
