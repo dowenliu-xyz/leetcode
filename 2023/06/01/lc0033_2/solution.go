281ba99c239857cd783ec8e4c2ed55566bc3b1b0
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-01 13:24:22 +0800

    WIP: leetcode 33, the second time

---
+++ 2023/06/01/lc0033_2/solution.go
@@ -0,0 +1,29 @@
+package lc0033_2
+
+func search(nums []int, target int) int {
+	n := len(nums)
+	if n == 0 {
+		return -1
+	}
+	l, r := 0, n-1
+	for l <= r {
+		m := l + (r-l)>>1
+		if nums[m] == target {
+			return m
+		}
+		if nums[l] <= nums[m] { // 注意 m 可能等于 l
+			if nums[l] <= target && target < nums[m] {
+				r = m - 1
+			} else {
+				l = m + 1
+			}
+		} else {
+			if nums[m] < target && target <= nums[r] {
+				l = m + 1
+			} else {
+				r = m - 1
+			}
+		}
+	}
+	return -1
+}
