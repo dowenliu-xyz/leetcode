2ecafd8da0409102d1325655550bb4238ddaf71f
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-01 13:06:27 +0800

    WIP: leetcode 16, the second time

---
+++ 2023/06/01/lc0016_2/solution.go
@@ -0,0 +1,44 @@
+package lc0016_2
+
+import "sort"
+
+func threeSumClosest(nums []int, target int) int {
+	n := len(nums)
+	if n < 3 {
+		return 0
+	}
+	sort.Ints(nums)
+	ans := nums[0] + nums[1] + nums[2]
+	for first := 0; first < n; first++ {
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		second, third := first+1, n-1
+		for second < third {
+			sum := nums[first] + nums[second] + nums[third]
+			if abs(target-ans) > abs(target-sum) {
+				ans = sum
+			}
+			if sum == target {
+				return sum
+			}
+			if sum < target {
+				for ; second < third && nums[second] == nums[second+1]; second++ {
+				}
+				second++
+			} else {
+				for ; second < third && nums[third] == nums[third-1]; third-- {
+				}
+				third--
+			}
+		}
+	}
+	return ans
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
---
+++ 2023/06/01/lc0016_2/solution_test.go
@@ -0,0 +1,60 @@
+package lc0016_2
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/dowenliu-xyz/leetcode/internal/inputs"
+)
+
+func Test_threeSumClosest(t *testing.T) {
+	tests := []struct {
+		input string
+		want  int
+	}{
+		{
+			input: `
+[-1,2,1,-4]
+1
+`,
+			want: 2,
+		},
+		{
+			input: `
+[0,0,0]
+1
+`,
+			want: 0,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			nums, target, err := readInput(tt.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got := threeSumClosest(nums, target); got != tt.want {
+				t.Errorf("threeSumClosest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func readInput(input string) (nums []int, target int, err error) {
+	lines := inputs.ReadNoneBlankLines(input)
+	if len(lines) != 2 {
+		err = fmt.Errorf("input should have 2 lines")
+		return
+	}
+	nums, err = inputs.ReadIntSlice(lines[0])
+	if err != nil {
+		return
+	}
+	target, err = strconv.Atoi(strings.TrimSpace(lines[1]))
+	if err != nil {
+		return
+	}
+	return
+}
