bb03ad5b8e1be34c0466071eb06e62f2f24faea4
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-05 14:00:37 +0800

    WIP: leetcode 239, the third time

--- 2023/06/05/lc0239_3/solution.go
+++ 2023/06/05/lc0239_3/solution.go
@@ -1,17 +1,21 @@
 package lc0239
 
 func maxSlidingWindow(nums []int, k int) []int {
-	var win []int
-	var ans []int
-	for i := 0; i < len(nums); i++ {
-		for len(win) > 0 && nums[win[len(win)-1]] <= nums[i] {
+	n := len(nums)
+	if n < k {
+		return nil
+	}
+	win := make([]int, 0, k)
+	ans := make([]int, 0, n-k)
+	for i, num := range nums {
+		for len(win) > 0 && nums[win[len(win)-1]] <= num {
 			win = win[:len(win)-1]
 		}
 		win = append(win, i)
 		if i < k-1 {
 			continue
 		}
-		if win[0] <= i-k {
+		for len(win) > 0 && win[0] < i-k+1 {
 			win = win[1:]
 		}
 		ans = append(ans, nums[win[0]])
