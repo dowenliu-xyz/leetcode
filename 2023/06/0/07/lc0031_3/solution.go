310a602c5c22e7961b7ee1ef0ae2e1517d11abfd
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-07 12:14:46 +0800

    WIP: leetcode 31, the third time

--- 2023/06/0/07/lc0031_3/solution.go
+++ 2023/06/0/07/lc0031_3/solution.go
@@ -2,27 +2,22 @@ package lc0031
 
 func nextPermutation(nums []int) {
 	n := len(nums)
-	if n < 2 {
+	if n == 0 {
 		return
 	}
 	i := n - 2
-	for i >= 0 {
-		if nums[i] < nums[i+1] {
-			break
-		}
-		i--
+	for ; i >= 0 && nums[i] >= nums[i+1]; i-- {
 	}
 	if i >= 0 {
 		j := n - 1
-		for j > i {
-			if nums[j] > nums[i] {
-				break
-			}
-			j--
+		for ; j > i && nums[i] >= nums[j]; j-- {
 		}
 		nums[i], nums[j] = nums[j], nums[i]
 	}
-	for l, r := i+1, n-1; l < r; l, r = l+1, r-1 {
+	l, r := i+1, n-1
+	for l < r {
 		nums[l], nums[r] = nums[r], nums[l]
+		l++
+		r--
 	}
 }
