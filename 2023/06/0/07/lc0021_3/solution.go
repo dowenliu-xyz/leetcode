d2a6cfcf47356089c31c10310ebabe92fb432450
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-07 08:56:52 +0800

    WIP: leetcode 21, the third time

--- 2023/06/0/07/lc0021_3/solution.go
+++ 2023/06/0/07/lc0021_3/solution.go
@@ -12,10 +12,11 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	pre := dummy
 	for list1 != nil && list2 != nil {
 		if list1.Val < list2.Val {
-			pre.Next, pre, list1 = list1, list1, list1.Next
+			pre.Next, list1 = list1, list1.Next
 		} else {
-			pre.Next, pre, list2 = list2, list2, list2.Next
+			pre.Next, list2 = list2, list2.Next
 		}
+		pre = pre.Next
 	}
 	if list1 != nil {
 		pre.Next = list1
