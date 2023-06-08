9f48470c19789b2711c0bad4f681438a8f7fec92
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-08 19:45:16 +0800

    WIP: leetcode 35, the third time

--- 2023/06/0/08/lc0035_3/solution.go
+++ 2023/06/0/08/lc0035_3/solution.go
@@ -4,10 +4,10 @@ func searchInsert(nums []int, target int) int {
 	l, r := 0, len(nums)-1
 	for l <= r {
 		m := l + (r-l)>>1
-		if nums[m] < target {
-			l++
+		if target <= nums[m] {
+			r = m - 1
 		} else {
-			r--
+			l = m + 1
 		}
 	}
 	return l
