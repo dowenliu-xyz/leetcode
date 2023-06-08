6e14f7dc5e715a986adb8754dd8f91a4e9c19e5b
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-08 13:37:27 +0800

    WIP: leetcode 88, the third time

--- 2023/06/0/08/lc0088_3/solution.go
+++ 2023/06/0/08/lc0088_3/solution.go
@@ -3,14 +3,11 @@ package lc0088
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	i, j, k := m-1, n-1, m+n-1
 	for i >= 0 && j >= 0 {
-		if nums1[i] < nums2[j] {
-			nums1[k] = nums2[j]
-			j--
+		if nums1[i] > nums2[j] {
+			nums1[k], i, k = nums1[i], i-1, k-1
 		} else {
-			nums1[k] = nums1[i]
-			i--
+			nums1[k], j, k = nums2[j], j-1, k-1
 		}
-		k--
 	}
 	if j >= 0 {
 		copy(nums1[:k+1], nums2[:j+1])
