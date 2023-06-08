6b460840be0fd50c54dfd518fc931563d6d4524b
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-08 22:27:11 +0800

    WIP: 剑指offer 5, the first time

---
+++ 2023/06/0/08/of0005_1/solution.go
@@ -0,0 +1,14 @@
+package of0005
+
+func replaceSpace(s string) string {
+	n := len(s)
+	ans := make([]byte, 0, n*3)
+	for i := 0; i < n; i++ {
+		if s[i] == ' ' {
+			ans = append(ans, "%20"...)
+		} else {
+			ans = append(ans, s[i])
+		}
+	}
+	return string(ans)
+}
