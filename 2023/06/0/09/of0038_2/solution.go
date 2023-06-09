1d7412bf282a77a8260b45bd2ca1a32a8f442bda
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-09 13:21:31 +0800

    WIP: 剑指offer 38, the second time

---
+++ 2023/06/0/09/of0038_2/solution.go
@@ -0,0 +1,29 @@
+package of0038
+
+func permutation(s string) []string {
+	n := len(s)
+	if n == 0 {
+		return nil
+	}
+	var ans []string
+	bs := []byte(s)
+	var search func(x int)
+	search = func(x int) {
+		if x == n-1 {
+			ans = append(ans, string(bs))
+			return
+		}
+		mem := make(map[byte]struct{})
+		for i := x; i < n; i++ {
+			if _, ok := mem[bs[i]]; ok {
+				continue
+			}
+			mem[bs[i]] = struct{}{}
+			bs[i], bs[x] = bs[x], bs[i]
+			search(x + 1)
+			bs[i], bs[x] = bs[x], bs[i]
+		}
+	}
+	search(0)
+	return ans
+}
