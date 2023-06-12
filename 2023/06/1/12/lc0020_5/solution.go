94b4d5f6fdb713b9e60b8617bddb7b79bd675f94
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-12 13:08:46 +0800

    WIP: leetcode 20, the fifth time

--- 2023/06/1/12/lc0020_5/solution.go
+++ 2023/06/1/12/lc0020_5/solution.go
@@ -6,16 +6,13 @@ func isValid(s string) bool {
 		'}': '{',
 		')': '(',
 	}
-	stack := make([]byte, 0, len(s))
+	var stack []byte
 	for i := 0; i < len(s); i++ {
-		if p, ok := pairs[s[i]]; ok {
-			if len(stack) == 0 || stack[len(stack)-1] != p {
-				return false
-			}
+		if len(stack) > 0 && stack[len(stack)-1] == pairs[s[i]] {
 			stack = stack[:len(stack)-1]
-		} else {
-			stack = append(stack, s[i])
+			continue
 		}
+		stack = append(stack, s[i])
 	}
 	return len(stack) == 0
 }
