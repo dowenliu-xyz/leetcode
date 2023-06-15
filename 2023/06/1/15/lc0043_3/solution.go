e4d4b4789e39e4be0d7a65ea84ba7e258966487d
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-15 19:26:44 +0800

    WIP: leetcode 43, the third time

---
+++ 2023/06/1/15/lc0043_3/solution.go
@@ -0,0 +1,28 @@
+package lc0043
+
+func multiply(num1 string, num2 string) string {
+	if num1 == "0" || num2 == "0" {
+		return "0"
+	}
+	m, n := len(num1), len(num2)
+	ans := make([]byte, m+n)
+	for i := m - 1; i >= 0; i-- {
+		x := num1[i] - '0'
+		for j := n - 1; j >= 0; j-- {
+			y := num2[j] - '0'
+			k := i + j + 1
+			tmp := x * y
+			for tmp != 0 {
+				ans[k] += tmp
+				ans[k], tmp, k = ans[k]%10, ans[k]/10, k-1
+			}
+		}
+	}
+	for len(ans) > 0 && ans[0] == 0 {
+		ans = ans[1:]
+	}
+	for i := range ans {
+		ans[i] += '0'
+	}
+	return string(ans)
+}
