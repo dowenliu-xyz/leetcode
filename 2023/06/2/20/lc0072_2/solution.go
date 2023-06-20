f8de1d232517a24e3ed0ac1959ae981849e2d2f2
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-20 13:55:39 +0800

    WIP: leetcode 72, the second time

---
+++ 2023/06/2/20/lc0072_2/solution.go
@@ -0,0 +1,36 @@
+package lc0072
+
+func minDistance(word1 string, word2 string) int {
+	m, n := len(word1), len(word2)
+	if m == 0 {
+		return n
+	}
+	if n == 0 {
+		return m
+	}
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+		dp[i][0] = i
+	}
+	for i := range dp[0] {
+		dp[0][i] = i
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j])) + 1
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
