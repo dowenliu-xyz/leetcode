6c521bd104e690b928aa2bcd0e5e8bda262b4d04
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-21 13:11:55 +0800

    WIP: leetcode 34, the third time

---
+++ 2023/06/2/21/lc0034_3/solution.go
@@ -0,0 +1,28 @@
+package lc0034
+
+func searchRange(nums []int, target int) []int {
+	ans := []int{-1, -1}
+	l, r := 0, len(nums)-1
+	for l <= r {
+		m := l + (r-l)>>1
+		if target <= nums[m] {
+			r = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+	if l >= len(nums) || nums[l] != target {
+		return ans
+	}
+	ans[0], r = l, len(nums)-1
+	for l <= r {
+		m := l + (r-l)>>1
+		if target >= nums[m] {
+			l = m + 1
+		} else {
+			r = m - 1
+		}
+	}
+	ans[1] = r
+	return ans
+}
