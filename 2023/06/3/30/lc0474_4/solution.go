6f806fe88f454ff926bb6076f0f3680340df0e93
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-30 13:41:17 +0800

    WIP: leetcode 474, the forth time

--- 2023/06/3/30/lc0474_4/solution.go
+++ 2023/06/3/30/lc0474_4/solution.go
@@ -3,13 +3,15 @@ package lc0474
 import "strings"
 
 func findMaxForm(strs []string, m int, n int) int {
+	// dp[i][j] 表示在最多有 i 个 0 和 j 个 1 时，最大子集长度
 	dp := make([][]int, m+1)
 	for i := range dp {
 		dp[i] = make([]int, n+1)
 	}
-	for _, s := range strs {
-		zeros := strings.Count(s, "0")
-		ones := len(s) - zeros
+	// 遍历元素，每个元素只能被使用一次
+	for _, str := range strs {
+		zeros := strings.Count(str, "0")
+		ones := len(str) - zeros
 		for i := m; i >= 0; i-- {
 			for j := n; j >= 0; j-- {
 				if i < zeros || j < ones {
