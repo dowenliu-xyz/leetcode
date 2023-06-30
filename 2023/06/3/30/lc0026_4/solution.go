8a30af494a38f53fa002474567698053ea95a471
dowenliu-xyz <hawkdowen@hotmail.com>
2023-06-30 13:33:54 +0800

    WIP: leetcode 26, the forth time

--- 2023/06/3/30/lc0026_4/solution.go
+++ 2023/06/3/30/lc0026_4/solution.go
@@ -1,13 +1,16 @@
 package lc0026
 
 func removeDuplicates(nums []int) int {
-	k := 0
-	for i := 0; i < len(nums); i++ {
+	k, n := 0, len(nums)
+	if n < 2 {
+		return n
+	}
+	for i := 0; i < n; i++ {
 		if nums[k] == nums[i] {
 			continue
 		}
 		k++
-		nums[i], nums[k] = nums[k], nums[i]
+		nums[k], nums[i] = nums[i], nums[k]
 	}
 	return k + 1
 }
