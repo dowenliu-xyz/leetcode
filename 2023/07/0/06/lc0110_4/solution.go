b35435969196ff19adc8f3cadca38227188289fb
dowenliu-xyz <hawkdowen@hotmail.com>
2023-07-06 08:59:25 +0800

    WIP: leetcode 110, the forth time

---
+++ 2023/07/0/06/lc0110_4/solution.go
@@ -0,0 +1,38 @@
+package lc0110
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isBalanced(root *TreeNode) bool {
+	return balancedHeight(root) != -1
+}
+
+func balancedHeight(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l, r := balancedHeight(root.Left), balancedHeight(root.Right)
+	if l == -1 || r == -1 || abs(l-r) > 1 {
+		return -1
+	}
+	return max(l, r) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
---
+++ 2023/07/0/06/lc0110_4/solution_test.go
@@ -0,0 +1,62 @@
+package lc0110
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/dowenliu-xyz/leetcode/internal/inputs"
+	"github.com/dowenliu-xyz/leetcode/internal/types"
+)
+
+func TestIsBalance(t *testing.T) {
+	tests := []struct {
+		input string
+		want  bool
+	}{
+		{
+			input: `[3,9,20,null,null,15,7]`,
+			want:  true,
+		},
+		{
+			input: `[1,2,2,3,3,null,null,4,4]`,
+			want:  false,
+		},
+		{
+			input: `[]`,
+			want:  true,
+		},
+		{
+			input: `[1,2,2,3,null,null,3,4,null,null,4]`,
+			want:  false,
+		},
+		{
+			input: `[1,2,3,4,5,6,null,8]`,
+			want:  true,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			node, err := inputs.LevelOrderRestore(tt.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			root := fromInternal(node)
+			got := isBalanced(root)
+			if tt.want != got {
+				t.Errorf("expect %t, got %t", tt.want, got)
+			}
+		})
+	}
+}
+
+func fromInternal(node *types.TreeNode) *TreeNode {
+	if node == nil {
+		return nil
+	}
+	n := &TreeNode{
+		Val:   node.Val,
+		Left:  fromInternal(node.Left),
+		Right: fromInternal(node.Right),
+	}
+	return n
+}
