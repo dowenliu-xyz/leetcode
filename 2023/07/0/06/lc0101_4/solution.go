55fdf885e3eefb364b0007c27b4623209f53d5b7
dowenliu-xyz <hawkdowen@hotmail.com>
2023-07-06 12:24:24 +0800

    WIP: leetcode 101, the forth time

---
+++ 2023/07/0/06/lc0101_4/solution.go
@@ -0,0 +1,47 @@
+package lc0101
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	//return isMirror(root, root)
+	return bfs(root)
+}
+
+func isMirror(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	if left.Val != right.Val {
+		return false
+	}
+	return isMirror(left.Left, right.Right) && isMirror(left.Right, right.Left)
+}
+
+func bfs(root *TreeNode) bool {
+	q := [][]*TreeNode{{root, root}}
+	for len(q) > 0 {
+		p := q[0]
+		q = q[1:]
+		l, r := p[0], p[1]
+		if l == nil && r == nil {
+			continue
+		}
+		if l == nil || r == nil {
+			return false
+		}
+		if l.Val != r.Val {
+			return false
+		}
+		q = append(q, []*TreeNode{l.Left, r.Right}, []*TreeNode{l.Right, r.Left})
+	}
+	return true
+}
