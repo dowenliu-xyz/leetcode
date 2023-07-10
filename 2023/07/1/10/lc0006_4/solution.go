958b093239d8d761c64cb6aee20151bcafd61da8
dowenliu-xyz <hawkdowen@hotmail.com>
2023-07-10 08:59:26 +0800

    WIP: leetcode 6, the forth time

---
+++ 2023/07/1/10/lc0006_4/solution.go
@@ -0,0 +1,22 @@
+package lc0006
+
+func convert(s string, numRows int) string {
+	if numRows < 2 {
+		return s
+	}
+	rows := make([][]byte, numRows)
+	row, dir := 0, -1
+	n := len(s)
+	for i := 0; i < n; i++ {
+		rows[row] = append(rows[row], s[i])
+		if row == 0 || row == numRows-1 {
+			dir = -dir
+		}
+		row += dir
+	}
+	ans := make([]byte, 0, n)
+	for i := range rows {
+		ans = append(ans, rows[i]...)
+	}
+	return string(ans)
+}
