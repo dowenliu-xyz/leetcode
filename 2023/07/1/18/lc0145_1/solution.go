b0e554a422194cf3c9dbec13862892d316e68e7a
dowenliu-xyz <hawkdowen@hotmail.com>
2023-07-18 17:36:02 +0800

    WIP: leetcode 145, the first time

---
+++ 2023/07/1/18/lc0145_1/solution.go
@@ -0,0 +1,56 @@
+package lc0145
+
+import (
+	. "github.com/dowenliu-xyz/leetcode/internal/types"
+)
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func postorderTraversal(root *TreeNode) []int {
+	//return solution1(root)
+	return solution2(root)
+}
+
+func solution1(root *TreeNode) []int {
+	var ans []int
+	var visit func(root *TreeNode)
+	visit = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		visit(root.Left)
+		visit(root.Right)
+		ans = append(ans, root.Val)
+	}
+	visit(root)
+	return ans
+}
+
+func solution2(root *TreeNode) []int {
+	var ans []int
+	var stack []*TreeNode
+	var pre *TreeNode
+	for root != nil || len(stack) > 0 {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if root.Right == nil || pre == root.Right {
+			ans = append(ans, root.Val)
+			pre = root
+			root = nil
+		} else {
+			stack = append(stack, root)
+			root = root.Right
+		}
+	}
+	return ans
+}
